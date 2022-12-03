aac445a305a17e41e7ad97ad88ec7d2e9dca0892
Sheldon <sheldonaldridge@yahoo.com>
2022-12-03 12:42:08 +0200

    Creating digimon data base
    The digiList array will contain all the digimon that the player can have and it's digivolution path and requirements

---
+++ js/digimon-digivolution.js
@@ -0,0 +1,41 @@
+let digiList = [
+
+    egg={
+        
+    },
+
+    inTraining={
+        botamon:{
+            id:"botamon",
+            name:"Botamon",
+            stage:"In-Training",
+            type:"Data",
+            digivolution:["Koromon"]
+        },
+
+        poyomon:{
+            id:"poyomon",
+            name:"Poyomon",
+            stage:"In-Training",
+            type:"Data",
+            digivolution:["Tokomon"]
+        },
+
+        punimon:{
+            id:"punimon",
+            name:"punimon",
+            stage:"In-Training",
+            type:"Data",
+            digivolution:["Tsunomon"]
+        },
+
+        punimon:{
+            id:"punimon",
+            name:"punimon",
+            stage:"In-Training",
+            type:"Data",
+            digivolution:["Tsunomon"]
+        },
+
+    }
+]
